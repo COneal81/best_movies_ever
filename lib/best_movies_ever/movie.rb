ff81816ac084dfeb92c19fb51400f6ed0825381c
Kari O'Neal <triadfirewood@gmail.com>
2019-03-15 21:31:14 +0000

    update

--- lib/best_movies_ever/movie.rb
+++ lib/best_movies_ever/movie.rb
@@ -13,7 +13,7 @@ class BestMoviesEver::Movie
       movie_1.name = "Black Panther"
       movie_1.genre = "Action & Adventure, Drama, Science Fiction & Fantasy"
       movie_1.rating = "PG"
-      movie_1.description = "Black Panther" follows wh0, after the events" 
+      movie_1.description = "Black Panther follows wh0, after the events" 
       movie_1.url = "https://www.rottentomatoes.com/m/black_panther_2018"
       
       movie_2 = self.new 
@@ -22,6 +22,7 @@ class BestMoviesEver::Movie
       movie_2.rating = "R"
       movie_2.description = "In Lady Bird,...beauty of a place called home."
       movie_2.url = "https://www.rottentomatoes.com/m/lady_bird"
+      [movie_1, movie_2]
     end
   
 end
