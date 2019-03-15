297275e17ce429ce7fe58798558d367c57fe335e
Kari O'Neal <triadfirewood@gmail.com>
2019-03-15 19:03:28 +0000

    update

--- lib/best_movies_ever.rb
+++ lib/best_movies_ever.rb
@@ -1,4 +1,5 @@
-require "best_movies_ever/version"
+require_relative "./best_movies_ever/version"
+require_relative "./best_movies_ever/cli"
 
 module BestMoviesEver
   class Error < StandardError; end
