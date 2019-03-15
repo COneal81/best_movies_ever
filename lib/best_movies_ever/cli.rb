c789e41b45153139cc4da4feb772947c1b95537f
Kari O'Neal <triadfirewood@gmail.com>
2019-03-15 17:58:11 +0000

    modified cli

---
+++ lib/best_movies_ever/cli.rb
@@ -0,0 +1,83 @@
+#The CLI is the controller for the user and the users interactions.  It should take in the users input.
+class BestMoviesEver::CLI 
+  
+  def call 
+    list_movies
+    menu
+  end
+
+  def list_movies
+   puts "Best Movies of All Time:"
+   puts <<-DOC #This will need to scrape all of the top 100 movies from RT.
+    1. Black Panther: Rated:PG, Genre: Action & Adventure, Drama, Science Fiction & Fantasy
+    2. Lady Bird:  Rated:R, Genre:Comedy, Drama
+    DOC
+  end
+
+#allow the user to choose all movies, movies by rating, movies by genre
+def menu
+    input = nil 
+    while input != 'exit'
+    puts "Please select a number of the movies that you would like to see;"
+    puts <<-DOC
+      1. View all top 100 movies.
+      2. View top movies by rating.
+      3. View top movies by genre.
+      DOC
+    
+    input = gets.strip
+    
+    case input 
+      when '1'
+        list_movies
+      when '2'
+        puts "Please select a rating"
+        puts "G"
+        puts "PG"
+        puts "PG-13"
+        puts "NC-17"
+        puts "R"
+        
+        selection = gets.strip
+          case selection
+            when 'G'
+              puts "Lists all of the G rated top 100 movies"
+            when 'PG'
+              puts "Lists all of the PG rated top 100 movies"
+            when 'PG-13'
+              puts "Lists all of the PG-13 rated top 100 movies"
+            when 'NC-17'
+              puts "Lists all of the NC-17 rated top 100 movies"
+            when 'R'
+              puts "Lists all of the R rated top 100 movies"
+            else puts "Invalid Entry"
+              menu
+            end
+        when '3'
+          puts "Please select a genre"
+          puts "Action & Adventure"
+          puts "Comedy"
+          puts "Drama"
+          puts "Fantasy"
+          puts "Science Fiction"
+          
+          choice = gets.strip
+          
+            case choice
+             when "Action & Adventure"
+               puts "All of the action and adventure movies"
+             when "Comedy"
+               puts "All of the Comedy movies"
+             when "Drama"
+               puts "All of the Drama movies"
+             when "Fantasy"
+               puts "All of the Fantasy movies"
+             when "Science Fiction"
+               puts "All of the Science Fiction movies"
+             else
+               puts "Invalid Entry"
+          menu
+        end
+  end
+
+end
