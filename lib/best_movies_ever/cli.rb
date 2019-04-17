476144d56984264c3a90ed1fee65ac07c5f0ca85
Kari O'Neal <triadfirewood@gmail.com>
2019-04-17 12:54:17 +0000

    Add color to cli

--- lib/best_movies_ever/cli.rb
+++ lib/best_movies_ever/cli.rb
@@ -3,12 +3,11 @@ class BestMoviesEver::CLI
   attr_accessor :movie_list
   
   def call 
-    puts "Welcome to Best Movies Ever CLI, home to the top rated movies!!".green
+    puts "Welcome to Best Movies Ever CLI, home to the top 100 movies!!".yellow.bold
     BestMoviesEver::Scraper.scrape_rt
     movie_list
     list_movies
     menu
-    goodbye
   end
     
     
@@ -28,7 +27,7 @@ class BestMoviesEver::CLI
   def menu
     input = nil 
     while input != 'exit'
-      puts "Enter a number for the movie you would like to see more information on or type list to see all of the top 100 movies."
+      puts "Enter a number for the movie you would like to see more information on, type list to see all of the top 100 movies, or exit to leave.".yellow.bold
     input = gets.strip.downcase
         
     if input.to_i > 0
@@ -36,22 +35,28 @@ class BestMoviesEver::CLI
           BestMoviesEver::Scraper.scrape_details(the_movie)
            puts ""
            puts "* * * * * * * * * * * * * * ~~  More Information ~~ * * * * * * * * * * * * * *".bold.red
-           puts "      Title: #{the_movie.name}".italic.bold.green
+           puts "      Title:#{the_movie.name}".green
           # puts "      URL:   #{the_movie.url}"
-           puts "      TV Rating: #{the_movie.rating}".bold.blue
-           puts "      Genre: #{the_movie.genre}".bold.yellow
-           puts "      Description: #{the_movie.description}".bold.cyan
-           puts "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ".bold.red
+           puts "      TV Rating: #{the_movie.rating}".blue
+           puts "      Genre: #{the_movie.genre}".magenta
+           puts "      Description: #{the_movie.description}".cyan
+           puts "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *".bold.red
       elsif input == "list"
         list_movies
+      elsif input == "exit"
+        goodbye
       else
-        puts "Invalid Entry. Enter a number  to see more information on a particular movie, list to see all movies, or exit to leave."
+        puts ""
+        puts "Invalid Entry.".underline.bold.red
+        puts ""
       end
     end
   end
     
+    
   def goodbye
-    puts "Thank you for visiting.  Check back with us when it is time to watch another movie!"
+    puts "Thank you for visiting!".bold.yellow 
+    puts "Check back with us when it is time to watch another movie!".bold.yellow
   end 
 
 end
